f8224244f95eeb4b078ea7e9c709dcde831996cc
Anatoliy <and1life@yandex.ru>
2026-04-23 23:37:44 +0300

    feat(Handler): added Parsing JSON in handle_register

--- src/handler.cpp
+++ src/handler.cpp
@@ -1,6 +1,8 @@
 #include "handler.h"
 #include <iostream>
+#include "json.hpp"
 
+using json = nlohmann::json;
 
 http::response<http::string_body> Handler::handle(const http::request<http::string_body> &req)
 {
@@ -41,21 +43,77 @@ http::response<http::string_body> Handler::handle_register(const http::request<h
         return res;
     }
 
-    std::string body = req.body();
+    json data;
 
-    std::cout << "Register body: " << body << std::endl;
+    try
+    {
+        data = json::parse(req.body());
+    }
+    catch(...)
+    {
+        http::response<http::string_body> res {
+            http::status::bad_request,
+            req.version()
+        };
 
+        res.set(http::field::content_type, "application/json");
+        res.body() = R"({
+            "success": false,
+            "error": "validation_error",
+            "message": "Invalid JSON"
+        })";
+
+        res.prepare_payload();
+        return res;
+    }
+
+    if (!data.contains("username") ||
+        !data.contains("email") ||
+        !data.contains("phone") ||
+        !data.contains("password"))
+    {
+        http::response<http::string_body> res {
+            http::status::bad_request,
+            req.version()
+        };
+
+        res.set(http::field::content_type, "application/json");
+        res.body() = R"({
+            "success": false,
+            "error": "validation_error",
+            "message": "Missing required fields"
+        })";
+
+        res.prepare_payload();
+        return res;
+    }
+
+
+    std::string username = data["username"];
+    std::string email = data["email"];
+    std::string phone = data["phone"];
+    std::string password = data["password"];
+
+    std::cout << "Parsed user: " << username << ", " << email << std::endl;
+    
     http::response<http::string_body> res {
         http::status::ok,
         req.version()
     };
 
     res.set(http::field::content_type, "application/json");
-    res.body() = R"({
-        "success": true,
-        "message": "register endpoint works"
-    })";
 
+    json response = {
+        {"success", true},
+        {"message", "JSON parsed successfully"},
+        {"user", {
+            {"username", username},
+            {"email", email}
+        }}
+    };
+
+    res.body() = response.dump();
     res.prepare_payload();
-    return res; 
+
+    return res;
 }
